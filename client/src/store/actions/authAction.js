c9f4e1045325a1b52703698fd6a0d4add18bc229
pqtran512 <117886346+pqtran512@users.noreply.github.com>
2024-01-31 19:34:19 +0700

    Update auth 31/1

--- client/src/store/actions/authAction.js
+++ client/src/store/actions/authAction.js
@@ -1,50 +1,50 @@
-// import actionTypes from './actionTypes'
-// import { apiRegister, apiLogin } from '../../services/authService'
+import actionTypes from './actionTypes'
+import { apiRegister, apiLogin } from '../../services/authService'
 
-// export const register = (payload) => async (dispatch) => { // register func. returns a func
-//     try {
-//         const response = await apiRegister(payload)
-//         if (response?.data.err === 0) {
-//             dispatch({
-//                 type: actionTypes.REGISTER_SUCESS,
-//                 data: response.data.token
-//             })
-//         } else {
-//             dispatch({
-//                 type: actionTypes.REGISTER_FAIL,
-//                 data: response.data.msg
-//             })
-//         }
-//     } catch (error) {
-//         dispatch({
-//             type: actionTypes.REGISTER_FAIL,
-//             data: null
-//         })
-//     }
-// }
+export const register = (payload) => async (dispatch) => { // register func. returns a func
+    try {
+        const response = await apiRegister(payload)
+        if (response?.data.err === 0) {
+            dispatch({
+                type: actionTypes.REGISTER_SUCESS,
+                data: response.data.token
+            })
+        } else {
+            dispatch({
+                type: actionTypes.REGISTER_FAIL,
+                data: response.data.msg
+            })
+        }
+    } catch (error) {
+        dispatch({
+            type: actionTypes.REGISTER_FAIL,
+            data: null
+        })
+    }
+}
 
-// export const login = (payload) => async (dispatch) => { // register func. returns a func
-//     try {
-//         const response = await apiLogin(payload)
-//         if (response?.data.err === 0) {
-//             dispatch({
-//                 type: actionTypes.LOGIN_SUCESS,
-//                 data: response.data.token
-//             })
-//         } else {
-//             dispatch({
-//                 type: actionTypes.LOGIN_FAIL,
-//                 data: response.data.msg
-//             })
-//         }
-//     } catch (error) {
-//         dispatch({
-//             type: actionTypes.LOGIN_FAIL,
-//             data: null
-//         })
-//     }
-// }
+export const login = (payload) => async (dispatch) => { // register func. returns a func
+    try {
+        const response = await apiLogin(payload)
+        if (response?.data.err === 0) {
+            dispatch({
+                type: actionTypes.LOGIN_SUCESS,
+                data: response.data.token
+            })
+        } else {
+            dispatch({
+                type: actionTypes.LOGIN_FAIL,
+                data: response.data.msg
+            })
+        }
+    } catch (error) {
+        dispatch({
+            type: actionTypes.LOGIN_FAIL,
+            data: null
+        })
+    }
+}
 
-// export const logout = () => ({
-//     type: actionTypes.LOGOUT
-// })
+export const logout = () => ({
+    type: actionTypes.LOGOUT
+})
