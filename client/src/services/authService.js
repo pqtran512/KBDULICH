da38d52c1d54f06018bdc7e64db90d5fea2867f0
pqtran512 <117886346+pqtran512@users.noreply.github.com>
2024-01-31 19:30:37 +0700

    Update auth 31/1

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,27 +1,27 @@
-// import axiosConfig from '../axiosConfig'
+import axiosConfig from '../axiosConfig'
 
-// export const apiRegister = (payload) => new Promise(async(resolve, reject) => {
-//     try {
-//         const response = await axiosConfig({
-//             method: 'post',
-//             url: '/api/v1/auth/register',
-//             data: payload // payload: email, password
-//         })
-//         resolve(response)
-//     } catch (error) {
-//         reject(error)
-//     }
-// })
+export const apiRegister = (payload) => new Promise(async(resolve, reject) => {
+    try {
+        const response = await axiosConfig({
+            method: 'post',
+            url: '/api/customer/create/',
+            data: payload // payload: email, password
+        })
+        resolve(response)
+    } catch (error) {
+        console.log(error)
+    }
+})
 
-// export const apiLogin = (payload) => new Promise(async(resolve, reject) => {
-//     try {
-//         const response = await axiosConfig({
-//             method: 'post',
-//             url: '/api/v1/auth/login',
-//             data: payload // payload: email, password
-//         })
-//         resolve(response)
-//     } catch (error) {
-//         reject(error)
-//     }
-// })
+export const apiLogin = (payload) => new Promise(async(resolve, reject) => {
+    try {
+        const response = await axiosConfig({
+            method: 'post',
+            url: '/api/customer/get',
+            data: payload // payload: email, password
+        })
+        resolve(response)
+    } catch (error) {
+        reject(error)
+    }
+})
