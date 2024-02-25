3e8ea43958526b009c6f4d4f729b28d04c1d6ac1
pqtran512 <117886346+pqtran512@users.noreply.github.com>
2024-02-25 17:29:11 +0700

    Update register api

--- backend/mydata/viewsData/customerView.py
+++ backend/mydata/viewsData/customerView.py
@@ -1,40 +1,74 @@
 from rest_framework import generics
 from mydata.models import Customer
-from mydata.serializers.customerSerializer import CustomerSerializer
+from mydata.serializers.customerSerializer import CustomerSerializer, CustomerLoginSerializer
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import views
 
-class CustomerDetail(generics.RetrieveAPIView):
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
+import logging
 
-    def getCustomer(self):
-        username = self.request.query_params.get('username', None)
-        password = self.request.query_params.get('password', None)
-        
-        if username is None or password is None:
-            raise NotFound("Username and password are required.")
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
+class CustomerDetail(views.APIView):
+    serializer_class = CustomerLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        username = self.request.data.get('email', None)
+        password = self.request.data.get('password', None)
 
         try:
-            return Customer.objects.get(username=username, password=password)
-        except Customer.DoesNotExist:
-            raise NotFound("Customer not found.")
+            if '@' in username:
+                customer = Customer.objects.get(email=username)
+            else: 
+                customer = Customer.objects.get(username=username)
 
-    def get(self, request, *args, **kwargs):
-        instance = self.getCustomer()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            if password != customer.password:
+                return Response({'err': 1, 'msg': 'Mật khẩu chưa chính xác !', 'token': None})
+            
+            serializer = self.serializer_class(customer)
+            return Response({'err': 0, 'msg': 'Đăng nhập thành công !', 'token': serializer.data}, status=status.HTTP_200_OK)
+        
+        except Customer.DoesNotExist:
+            return Response({'err': 1, 'msg': 'Không tìm thấy người dùng !', 'token': None})
     
 class CustomerCreate(generics.CreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    msg = ''
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        msg = 'error'
 
-    def perform_create(self, serializer):
-        serializer.createCustomer(serializer.validated_data)
+        if serializer.is_valid():
+            username = self.request.data.get('username')
+            phone_no = self.request.data.get('phone_no')
+            email = self.request.data.get('email')
+
+            new_customer = Customer.objects.create(
+                    username = username,
+                    password = request.data['password'],
+                    phone_no = phone_no,
+                    email    = email
+                )
+
+            serializer = self.get_serializer(new_customer)
+            response_data = {'err': 0, 'msg': 'success', 'token': serializer.data}
+            return Response(response_data, status=status.HTTP_201_CREATED)
+                
+            
+        else:
+            errors = serializer.errors
+            tmp = []
+            if 'username' in errors:
+                tmp.append('Tên người dùng đã tồn tại !')
+            elif 'email' in errors:
+                tmp.append('Email đã được sử dụng !')
+            elif 'phone_no' in errors:
+                tmp.append('Số điện thoại đã được sử dụng !')
+            
+            msg = ' '.join(tmp)
+
+            return Response({'error': 1, 'msg': msg, 'token': None})
